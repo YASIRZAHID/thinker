69cca621f64acac517d8c558d6bf098072af023b
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2016-06-21 15:56:15 -0400

    style: Improve Snackbar dark mode support

---
+++ src/components/Snackbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Snackbar.module.css';
+
+interface SnackbarProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Snackbar: React.FC<SnackbarProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.styledContainer;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
