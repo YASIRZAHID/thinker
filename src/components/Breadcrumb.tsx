f672f27868e5f144f914f80aa6743bded5dff1d8
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2016-11-14 14:30:15 -0500

    docs: Update Breadcrumb documentation

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Breadcrumb.module.css';
+
+interface BreadcrumbProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Breadcrumb: React.FC<BreadcrumbProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
