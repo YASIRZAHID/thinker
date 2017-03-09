9024ca855c8b4685dddfc1cb9671751a23ef22a9
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2017-03-09 17:54:15 -0500

    docs: Add storybook stories for Skeleton

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Skeleton.module.css';
+
+interface SkeletonProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Skeleton: React.FC<SkeletonProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
