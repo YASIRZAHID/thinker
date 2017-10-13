3eaab6f69f18c636c52b62cdf4db097671d9f436
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2017-10-13 13:22:15 -0400

    feat: Add AspectRatio integration with backend

---
+++ src/components/AspectRatio.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './AspectRatio.module.css';
+
+interface AspectRatioProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const AspectRatio: React.FC<AspectRatioProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
