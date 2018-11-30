75afc9a2446fae9994c5687566451ee4936b9284
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2018-11-30 09:12:15 -0500

    chore: Optimize RadarChart bundle size

---
+++ src/components/RadarChart.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './RadarChart.module.css';
+
+interface RadarChartProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const RadarChart: React.FC<RadarChartProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
