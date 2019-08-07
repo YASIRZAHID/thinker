a60024d3f6c06ab7b81929fc373994ba9266b85f
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2019-08-07 13:08:15 -0400

    feat: Add IconButton integration with backend

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { theme } from '../theme';
+
+const fadeIn = keyframes`
+    from { opacity: 0; }
+    to { opacity: 1; }
+`;
+
+const StyledIconButton = styled(motion.div)`
+    padding: ${theme.spacing.medium};
+    border-radius: ${theme.borderRadius};
+    background: ${theme.colors.background};
+    box-shadow: ${theme.shadows.medium};
+    transition: all 0.3s ease;
+    animation: ${fadeIn} 0.3s ease-in;
+    
+    &:hover {
+        transform: translateY(-2px);
+        box-shadow: ${theme.shadows.large};
+    }
+`;
+
+export const IconButton = () => {
+    return (
+        <StyledIconButton
+            initial={ opacity: 0 }
+            animate={ opacity: 1 }
+            exit={ opacity: 0 }
+        >
+            <div className="content">
+                {component} Content Here
+            </div>
+        </StyledIconButton>
+    );
+};
