b725186a2963725463c5e606eddb9dc1714c58c4
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2020-03-05 16:02:00 -0500

    docs: Add storybook stories for FileUpload

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './FileUpload.module.css';
+
+interface FileUploadProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const FileUpload: React.FC<FileUploadProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
