7ef70efea5feaed88f525e2236248fca8bf4a5a6
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2021-09-22 09:27:00 -0400

    feat: Add DataGrid component with animation effects support

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -18,7 +18,7 @@ export const DataGrid: React.FC<DataGridProps> = ({
     children,
     onClick 
 }) => {
-    const baseClass = styles.styledContainer;
+    const baseClass = styles.container;
     const variantClass = styles[variant];
     const sizeClass = styles[size];
     
