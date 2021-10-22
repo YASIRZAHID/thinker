11dfb4dc4033654a61bd2f091f4782e1a73ca407
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2021-10-22 16:23:15 -0400

    fix: Resolve Tag state management bug

---
+++ src/components/Tag.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Tag.module.css';
+
+interface TagProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Tag: React.FC<TagProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
