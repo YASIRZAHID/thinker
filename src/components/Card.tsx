6ed79565b02efde741a3e9fd309adecbc1fedb09
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2021-12-23 11:53:00 -0500

    refactor: Optimize Card component performance

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,7 +18,7 @@ export const Card: React.FC<CardProps> = ({
     children,
     onClick 
 }) => {
-    const baseClass = styles.styledContainer;
+    const baseClass = styles.container;
     const variantClass = styles[variant];
     const sizeClass = styles[size];
     
