a3345255794cb08d7cfbb65245178815a58ab6be
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2022-04-26 14:12:15 -0400

    fix: Fix Sidebar accessibility issues

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Sidebar.module.css';
+
+interface SidebarProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
