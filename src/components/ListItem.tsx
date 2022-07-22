463d8224ec78a92b68bfcb86f2af4559908ede3f
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2022-07-22 14:57:15 -0400

    style: Update ListItem theme compatibility

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,36 +1,34 @@
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { theme } from '../theme';
+import React from 'react';
+import styles from './ListItem.module.css';
 
-const fadeIn = keyframes`
-    from { opacity: 0; }
-    to { opacity: 1; }
-`;
+interface ListItemProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
 
-const StyledListItem = styled(motion.div)`
-    padding: ${theme.spacing.medium};
-    border-radius: ${theme.borderRadius};
-    background: ${theme.colors.background};
-    box-shadow: ${theme.shadows.medium};
-    transition: all 0.3s ease;
-    animation: ${fadeIn} 0.3s ease-in;
+export const ListItem: React.FC<ListItemProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.styledContainer;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
     
-    &:hover {
-        transform: translateY(-2px);
-        box-shadow: ${theme.shadows.large};
-    }
-`;
-
-export const ListItem = () => {
     return (
-        <StyledListItem
-            initial={ opacity: 0 }
-            animate={ opacity: 1 }
-            exit={ opacity: 0 }
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
         >
-            <div className="content">
-                {component} Content Here
-            </div>
-        </StyledListItem>
+            {children}
+        </div>
     );
 };
