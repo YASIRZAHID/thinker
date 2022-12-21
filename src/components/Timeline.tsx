28bc5ee575e9603e39d0f0f3518571e5ea5832f6
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2022-12-21 12:15:15 -0500

    feat: Add Timeline component with theme support support

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Timeline.module.css';
+
+interface TimelineProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Timeline: React.FC<TimelineProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
