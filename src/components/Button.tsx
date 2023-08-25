0f70d8d2e8e494e1ba04a4731f0dd31e63571f97
YASIRZAHID <SCIENTEST5661@GMAIL.COM>
2023-08-25 12:17:00 -0400

    feat: Add Button component with animation effects support

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ 
+    variant = 'primary',
+    size = 'medium',
+    disabled = false,
+    className,
+    children,
+    onClick 
+}) => {
+    const baseClass = styles.container;
+    const variantClass = styles[variant];
+    const sizeClass = styles[size];
+    
+    return (
+        <div 
+            className={`${baseClass} ${variantClass} ${sizeClass} ${className}`}
+            onClick={disabled ? undefined : onClick}
+            aria-disabled={disabled}
+        >
+            {children}
+        </div>
+    );
+};
